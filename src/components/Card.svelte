087b5c1eed56a29542b55a089785729b4ce0588b
fusbudget <andrewsanser@gmail.com>
2022-06-30 17:52:58 +0200

    add Cards (grey rectangles) and Home page

---
+++ src/components/Card.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  export let width;
+  export let height;
+</script>
+
+<div class="bg-slate-500 {width} {height}"></div>
---
+++ src/components/Home.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Card from './Card.svelte';
+</script>
+
+<h1>Home</h1>
+<div class="flex justify-around h-48">
+  <Card width={"w-1/5"} height={"min-h-full"} />
+  <Card width={"w-1/5"} height={"min-h-full"} />
+  <Card width={"w-1/5"} height={"min-h-full"} />
+  <Card width={"w-1/5"} height={"min-h-full"} />
+</div>
