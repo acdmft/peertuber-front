977339a8e4ab8c44845a6c964025e2132bad4cdc
fusbudget <andrewsanser@gmail.com>
2022-07-01 17:28:44 +0200

    add styles to the Cards: video title, description, image

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,6 +1,27 @@
 <script lang="ts">
   export let width;
   export let height;
+  export let img;
+  export let channel_name;
+  export let channel_url;
+  export let video_title;
+  export let video_url;
 </script>
 
-<div class="bg-slate-500 {width} {height}"></div>
+<div class="bg-slate-500 {width} {height}">
+  <!----------    CHANNEL NAME AND URL    ----------->
+  <div class="h-10 bg-amber-300 text-left px-2 leading-none">
+    <span class="text-sm text-gray-500 font-semibold ">{channel_name}</span>
+    <span class="text-xs text-gray-500 font-semibold ">{channel_url}</span>
+  </div>
+  <img src={img} alt="video preview" class="w-full" />
+  <!-----------    VIDEO DESCRIPTION      ---------->
+  <div class="bg-green-300 h-20">
+    <div class="text-left text-gray-600 font-semibold text-base px-2">
+      <a href={video_url}>
+        {video_title}
+      </a>
+    </div>
+  </div>
+
+</div>
--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,11 +1,43 @@
 <script lang="ts">
-  import Card from './Card.svelte';
+  import Card from "./Card.svelte";
 </script>
 
 <h1>Home</h1>
-<div class="flex justify-around h-48">
-  <Card width={"w-1/5"} height={"min-h-full"} />
-  <Card width={"w-1/5"} height={"min-h-full"} />
-  <Card width={"w-1/5"} height={"min-h-full"} />
-  <Card width={"w-1/5"} height={"min-h-full"} />
+<div class="flex justify-around h-64">
+  <Card
+    width={"w-1/5"}
+    height={"min-h-full"}
+    img={"https://gary.vger.cloud/static/thumbnails/073354a9-1a0d-4c79-86af-e427dcc57818.jpg"}
+    channel_name={"Gary Seven - Assignment: Earth"}
+    channel_url={"gary.vger.cloud"}
+    video_title={"Big Buck Bunny 60fps 4K - Official Blender Foundation Short Film"}
+    video_url={"https://gary.vger.cloud/videos/watch/6402b77c-b61f-4a06-96ca-c8420a2becf4"}
+  />
+  <Card
+    width={"w-1/5"}
+    height={"min-h-full"}
+    img={"https://gary.vger.cloud/static/thumbnails/073354a9-1a0d-4c79-86af-e427dcc57818.jpg"}
+    channel_name={"Gary Seven - Assignment: Earth"}
+    channel_url={"gary.vger.cloud"}
+    video_title={"Big Buck Bunny 60fps 4K - Official Blender Foundation Short Film"}
+    video_url={"https://gary.vger.cloud/videos/watch/6402b77c-b61f-4a06-96ca-c8420a2becf4"}
+  />
+  <Card
+    width={"w-1/5"}
+    height={"min-h-full"}
+    img={"https://gary.vger.cloud/static/thumbnails/073354a9-1a0d-4c79-86af-e427dcc57818.jpg"}
+    channel_name={"Gary Seven - Assignment: Earth"}
+    channel_url={"gary.vger.cloud"}
+    video_title={"Big Buck Bunny 60fps 4K - Official Blender Foundation Short Film"}
+    video_url={"https://gary.vger.cloud/videos/watch/6402b77c-b61f-4a06-96ca-c8420a2becf4"}
+  />
+  <Card
+    width={"w-1/5"}
+    height={"min-h-full"}
+    img={"https://gary.vger.cloud/static/thumbnails/073354a9-1a0d-4c79-86af-e427dcc57818.jpg"}
+    channel_name={"Gary Seven - Assignment: Earth"}
+    channel_url={"gary.vger.cloud"}
+    video_title={"Big Buck Bunny 60fps 4K - Official Blender Foundation Short Film"}
+    video_url={"https://gary.vger.cloud/videos/watch/6402b77c-b61f-4a06-96ca-c8420a2becf4"}
+  />
 </div>
