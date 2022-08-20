d3b03e8bfeb31fbe470ca032d0e78a9670d7dc9d
fusbudget <andrewsanser@gmail.com>
2022-08-20 19:44:59 +0200

    connect frontend to backend with onMount and fetch functions in Home component

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -2,6 +2,30 @@
   import VideoRow from "./VideoRow.svelte";
   import Header from "./Header.svelte";
   import LeftSidebar from "./LeftSidebar.svelte";
+  // onMount
+  import { onMount } from 'svelte';
+
+  const api_url = import.meta.env.VITE_API_URL;
+  let videos = [];
+  // $: loadedVideos = videos.length;
+  
+  onMount(async () => {
+    const query = { "query": `{ videos { instance name url thumbnailImg }}`};
+    const res = await fetch(`${api_url}/data`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(query)
+    });
+    const result = await res.json();
+    console.log(result.data.videos);
+    // videos.push(result.data.videos);
+    videos = result.data.videos;
+
+  });
+
+
 </script>
 
 <!---------       TOP MENU            ----------->
@@ -13,7 +37,14 @@
 <!----------         LEFT SEPARATOR    ------------->
 <!-- <div class="h-screen w-16" ></div> -->
 <!-----------       VIDEOROWS       ------------->
-<VideoRow />
-<VideoRow />
-<VideoRow />
+{#if videos.length !== 0}
+  <VideoRow cardsData={ videos.slice(0,4) } />
+  <VideoRow cardsData={ videos.slice(4,8) } />
+  <VideoRow cardsData={ videos.slice(8,12) } />
+{:else}
+  <p class="mt-10">
+
+    loading...
+  </p>
+{/if}
 
--- src/components/VideoRow.svelte
+++ src/components/VideoRow.svelte
@@ -1,42 +1,49 @@
 <script lang="ts">
   import Card from "./Card.svelte";
+  export let cardsData;
+  // export let imgUrl: String;
+  // export let instName: String;
+  // export let videoTitle: String;
+  // export let videoUrl: String;
+
+
 </script>
 
 <div class="flex justify-around h-62 mt-12 w-6/8 ml-12">
   <Card
     width={"w-1/5"}
     height={"min-h-full"}
-    img={"https://gary.vger.cloud/static/thumbnails/edb9ae22-2eb7-429b-acec-cc3882d3a1f3.jpg"}
-    channel_name={"Gary Seven - Assignment: Earth"}
-    channel_url={"gary.vger.cloud"}
-    video_title={"Big Buck Bunny 60fps 4K - Official Blender Foundation Short Film"}
-    video_url={"https://gary.vger.cloud/videos/watch/6402b77c-b61f-4a06-96ca-c8420a2becf4"}
+    img={ `http://${cardsData[0].instance}${cardsData[0].thumbnailImg}` }
+    channel_name={ cardsData[0].instance }
+    channel_url={ cardsData[0].instance }
+    video_title={ cardsData[0].name }
+    video_url={ cardsData[0].url }
   />
   <Card
     width={"w-1/5"}
     height={"min-h-full"}
-    img={"https://gary.vger.cloud/static/thumbnails/edb9ae22-2eb7-429b-acec-cc3882d3a1f3.jpg"}
-    channel_name={"Gary Seven - Assignment: Earth"}
-    channel_url={"gary.vger.cloud"}
-    video_title={"Big Buck Bunny 60fps 4K - Official Blender Foundation Short Film"}
-    video_url={"https://gary.vger.cloud/videos/watch/6402b77c-b61f-4a06-96ca-c8420a2becf4"}
+    img={ `http://${cardsData[1].instance}${cardsData[1].thumbnailImg}` }
+    channel_name={ cardsData[1].instance }
+    channel_url={ cardsData[1].instance }
+    video_title={ cardsData[1].name }
+    video_url={ cardsData[1].url }
   />
   <Card
     width={"w-1/5"}
     height={"min-h-full"}
-    img={"https://gary.vger.cloud/static/thumbnails/073354a9-1a0d-4c79-86af-e427dcc57818.jpg"}
-    channel_name={"Gary Seven - Assignment: Earth"}
-    channel_url={"gary.vger.cloud"}
-    video_title={"Big Buck Bunny 60fps 4K - Official Blender Foundation Short Film"}
-    video_url={"https://gary.vger.cloud/videos/watch/6402b77c-b61f-4a06-96ca-c8420a2becf4"}
+    img={ `http://${cardsData[2].instance}${cardsData[2].thumbnailImg}` }
+    channel_name={ cardsData[2].instance }
+    channel_url={ cardsData[2].instance }
+    video_title={ cardsData[2].name }
+    video_url={ cardsData[2].url }
   />
   <Card
     width={"w-1/5"}
     height={"min-h-full"}
-    img={"https://gary.vger.cloud/static/thumbnails/073354a9-1a0d-4c79-86af-e427dcc57818.jpg"}
-    channel_name={"Gary Seven - Assignment: Earth"}
-    channel_url={"gary.vger.cloud"}
-    video_title={"Big Buck Bunny 60fps 4K - Official Blender Foundation Short Film"}
-    video_url={"https://gary.vger.cloud/videos/watch/6402b77c-b61f-4a06-96ca-c8420a2becf4"}
+    img={ `http://${cardsData[3].instance}${cardsData[3].thumbnailImg}` }
+    channel_name={ cardsData[3].instance }
+    channel_url={ cardsData[3].instance }
+    video_title={ cardsData[3].name }
+    video_url={ cardsData[3].url }
   />
 </div>
