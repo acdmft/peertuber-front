3b2facc1c0ab802555eb24c492671249ea6c2cf3
fusbudget <andrewsanser@gmail.com>
2022-08-30 19:17:54 +0200

    add min-height to video preview <a>channel title</a>

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -10,8 +10,8 @@
 
 <div class="bg-green-300 {width} {height}">
   <!----------    CHANNEL NAME AND URL    ----------->
-  <div class="h-12 bg-amber-300 text-left px-2 leading-none flex justify-center">
-    <a href={`https://${channel_url}`} class="self-center" target="_blank">{channel_name}</a>
+  <div class="h-12 bg-amber-300 text-left px-2 leading-none flex justify-center  overflow-hidden text-ellipsis">
+    <a href={`https://${channel_url}`} class="self-center max-h-full" target="_blank">{channel_name}</a>
   </div>
   <div class="h-36 z-0 text-slate-600 flex relative text-5xl">
     <i class="fa-solid fa-film mx-auto self-center" />
