ca80c77a1947fc4a2f9efef79594300aad27cd93
fusbudget <andrewsanser@gmail.com>
2022-09-07 13:04:59 +0200

    add icons to LeftSidebar comp

--- src/components/LeftSidebar.svelte
+++ src/components/LeftSidebar.svelte
@@ -3,5 +3,38 @@
 </script>
 
 <div class="h-screen w-16 fixed left-0 top-0 bg-zinc-800"> 
+  <!-- STRIP BLOCK (GAP) -->
+  <div class="h-32"></div>
+  <!-- ICONS -->
+  <div class="text-slate-200 mb-2 hover:bg-zinc-700 hover:cursor-pointer text-2xl h-16">
+    <button>
+      <i class="fa-regular fa-clock" />
+    </button>
+    <div class="text-xs px-1">Watch later</div>
+  </div>
+  <div class="text-slate-200 mb-2 hover:bg-zinc-700 hover:cursor-pointer text-2xl h-16">
+    <button>
+      <i class="fa-regular fa-heart" />
+    </button>
+    <div class="text-xs ">Favorite</div>
+  </div>
+  <div class="text-slate-200 mb-2 hover:bg-zinc-700 hover:cursor-pointer text-2xl h-16">
+    <button>
+      <i class="fa-regular fa-folder-closed" />
+    </button>
+    <div class="text-xs ">Library</div>
+  </div>
+  <div class="text-slate-200 mb-2 hover:bg-zinc-700 hover:cursor-pointer text-2xl h-16">
+    <button>
+      <i class="fa-regular fa-envelope" />
+    </button>
+    <div class="text-xs ">Contact</div>
+  </div>
+  <div class="text-slate-200 mb-2 hover:bg-zinc-700 hover:cursor-pointer text-2xl h-16">
+    <button>
+      <i class="fa-solid fa-info" />
+    </button>
+    <div class="text-xs ">About</div>
+  </div>
 
 </div>
