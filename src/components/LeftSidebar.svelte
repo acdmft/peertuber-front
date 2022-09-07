706929d3aa1fb77537c0b46b963707df3c127f88
fusbudget <andrewsanser@gmail.com>
2022-09-07 11:41:23 +0200

    connect user store to localStorage

--- src/components/LeftSidebar.svelte
+++ src/components/LeftSidebar.svelte
@@ -3,5 +3,6 @@
 </script>
 
 <div class="h-screen w-16 fixed left-0 top-0 bg-zinc-800"> 
+  
 
 </div>
--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,6 +1,8 @@
 import { writable } from 'svelte/store';
 
 export const count = writable(0);
+// user's state (logged in or not)
+export const user = writable<boolean>(localStorage.user === 'true');
+user.subscribe((val) => localStorage.user = String(val));
 
-export const user = writable(false);
 export const accForms = writable(false);
