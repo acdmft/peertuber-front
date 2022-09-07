e8396d4ff353fc8314ec66e1246333554f9cc8f6
fusbudget <andrewsanser@gmail.com>
2022-09-07 15:51:53 +0200

    add Header and LeftSidebar components in About, Library, Liked, WatchLater views

--- src/views/Library.svelte
+++ src/views/Library.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
-  
+  import Header from "../components/Header.svelte";
+  import LeftSidebar from "../components/LeftSidebar.svelte";
 </script>
 
 <h1>Library</h1>
+<!---------       TOP MENU            ----------->
+<Header />
+<!-----------      TOP SEPARATOR       ------------>
+<div class="h-20" />
+<!-----------       LEFT MENU       ------------>
+<LeftSidebar />
