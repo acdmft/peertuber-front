316a913722c879804240a20ab83bf6a218e363cc
fusbudget <andrewsanser@gmail.com>
2022-09-07 16:28:24 +0200

    add Contact view

---
+++ src/views/Contact.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Header from "../components/Header.svelte";
+  import LeftSidebar from "../components/LeftSidebar.svelte";
+</script>
+
+<h1>Contact</h1>
+<!---------       TOP MENU            ----------->
+<Header />
+<!-----------      TOP SEPARATOR       ------------>
+<div class="h-20" />
+<!-----------       LEFT MENU       ------------>
+<LeftSidebar />
