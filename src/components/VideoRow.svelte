a62dc3911d909112f4b5fc6b5b471afb9c82880c
fusbudget <andrewsanser@gmail.com>
2022-09-08 10:24:54 +0200

    improve VideoRow so that it use each Svelte block

--- src/components/VideoRow.svelte
+++ src/components/VideoRow.svelte
@@ -4,56 +4,19 @@
 </script>
 
 <div class="flex justify-around h-62 mt-12 w-6/8 ml-12">
-  <Card
-    width={"w-1/5"}
-    height={"min-h-full"}
-    img={ `http://${cardsData[0].instance.host}${cardsData[0].thumbnailImg}` }
-    channel_name={ cardsData[0].instance.name }
-    channel_url={ cardsData[0].instance.host }
-    video_title={ cardsData[0].name }
-    video_url={ cardsData[0].url }
-    video_id={ cardsData[0]._id}
-    likes={cardsData[0].likes}
-    duration={cardsData[0].duration}
-    on:like
-  />
-  <Card
-    width={"w-1/5"}
-    height={"min-h-full"}
-    img={ `http://${cardsData[1].instance.host}${cardsData[1].thumbnailImg}` }
-    channel_name={ cardsData[1].instance.name }
-    channel_url={ cardsData[1].instance.host }
-    video_title={ cardsData[1].name }
-    video_url={ cardsData[1].url }
-    video_id={ cardsData[1]._id}
-    likes={cardsData[1].likes}
-    duration={cardsData[1].duration}
-    on:like
-  />
-  <Card
-    width={"w-1/5"}
-    height={"min-h-full"}
-    img={ `http://${cardsData[2].instance.host}${cardsData[2].thumbnailImg}` }
-    channel_name={ cardsData[2].instance.name }
-    channel_url={ cardsData[2].instance.host }
-    video_title={ cardsData[2].name }
-    video_url={ cardsData[2].url }
-    video_id={ cardsData[2]._id}
-    likes={cardsData[2].likes}
-    duration={cardsData[2].duration}
-    on:like
-  />
-  <Card
-    width={"w-1/5"}
-    height={"min-h-full"}
-    img={ `http://${cardsData[3].instance.host}${cardsData[3].thumbnailImg}` }
-    channel_name={ cardsData[3].instance.name }
-    channel_url={ cardsData[3].instance.host }
-    video_title={ cardsData[3].name }
-    video_url={ cardsData[3].url }
-    video_id={ cardsData[3]._id}
-    likes={cardsData[3].likes}
-    duration={cardsData[3].duration}
-    on:like
-  />
+  {#each cardsData as card}
+    <Card
+      width={"w-1/5"}
+      height={"min-h-full"}
+      img={`http://${card.instance.host}${card.thumbnailImg}`}
+      channel_name={card.instance.name}
+      channel_url={card.instance.host}
+      video_title={card.name}
+      video_url={card.url}
+      video_id={card._id}
+      likes={card.likes}
+      duration={card.duration}
+      on:like
+    />
+  {/each}
 </div>
