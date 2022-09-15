541cc863e7f6cfac684b51c69cd5e033004c7dc9
fusbudget <andrewsanser@gmail.com>
2022-09-15 12:53:37 +0200

    add ReportForm component

---
+++ src/components/ReportForm.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+  // EXPORTS 
+  export let video_id: string;
+  let report = "";
+  $: btnDisabled = report.length === 0 ? "disabled" : "";
+// FORM STATE
+let menuEnabled = true;
+  $: showForm = menuEnabled;
+  
+  function reportVideo() {
+    console.log(report, video_id);
+menuEnabled = false;
+  }
+</script>
+{#if showForm}
+
+<div
+  class="menu_wrapper flex justify-center h-screen w-full fixed top-0 left-10 z-10"
+>
+  <div class="flex items-center">
+    <!-----      MENU      ------>
+    <div class="w-fit h-fit bg-gray-900 rounded-md text-slate-400 px-4 pt-4">
+      <h3 class="font-semibold">Report </h3>
+
+      <form on:submit|preventDefault={reportVideo}>
+        <div class="mt-2">
+          <input
+            type="radio"
+            bind:group={report}
+            id="prn"
+            name="report"
+            value={"prn"}
+          />
+          <label class="ml-2" for="prn">Pornography</label>
+        </div>
+
+        <div class="mt-2">
+          <input
+            type="radio"
+            bind:group={report}
+            id="discr"
+            name="report"
+            value={"discr"}
+          />
+          <label class="ml-2" for="discr">Hateful, discriminatory content</label
+          >
+        </div>
+
+        <div class="mt-2">
+          <input
+            type="radio"
+            bind:group={report}
+            id="viol"
+            name="report"
+            value={"viol"}
+          />
+          <label class="ml-2" for="viol">Violent or repulsive content</label>
+        </div>
+        <!-- BUTTONS -->
+        <div class="my-4 flex justify-around">
+          <button on:click class="text-neutral-200 hover:text-red-200">Cancel</button>
+          {#if btnDisabled}
+            <button class="text-slate-600" disabled type="submit">Report</button
+            >
+          {:else}
+            <button class="text-orange-400" type="submit">Report</button>
+          {/if}
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+{/if}
+
+<style>
+  .menu_wrapper {
+    background-color: rgba(24, 24, 27, 0.75);
+  }
+</style>
