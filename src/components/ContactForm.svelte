b45b1ab2a221c044efb399df82a510c6b21ba818
fusbudget <andrewsanser@gmail.com>
2022-09-21 12:59:50 +0200

    add ContactForm comp

---
+++ src/components/ContactForm.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+</script>
+
+<form class="flex flex-col justify-around text-neutral-200 w-1/2 min-h-[400px] font-bold  border-2 border-solid border-neutral-400 rounded-lg">
+  <h1 class="font-bold text-xl">Contact us</h1>
+  <input type="email" id="email" placeholder="email" class="w-5/6 text-neutral-600 mx-auto rounded-md h-10 pl-2">
+  <input type="text" placeholder="name" class="w-5/6 text-neutral-600 mx-auto rounded-md h-10 pl-2">
+  <input type="text" placeholder="subject" class="w-5/6 text-neutral-600  mx-auto rounded-md h-10 pl-2" id="subject">
+  <textarea class="mt-2 h-24 text-black w-5/6 mx-auto rounded-md">
+  </textarea>
+<button type="submit" class="w-5/6 mx-auto mt-3 mb-4 rounded-md py-2 hover:bg-blue-700 text-lg font-bold  bg-blue-600">Send</button>
+</form>
