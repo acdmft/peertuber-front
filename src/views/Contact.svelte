44ada2662ff401e659eee88f02d020c58d181d1b
fusbudget <andrewsanser@gmail.com>
2022-09-21 13:00:37 +0200

    add contact info and form in Contact page

--- src/views/Contact.svelte
+++ src/views/Contact.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import Header from "../components/Header.svelte";
   import LeftSidebar from "../components/LeftSidebar.svelte";
+  import ContactForm from "../components/ContactForm.svelte";
 </script>
 
 <h1>Contact</h1>
@@ -10,3 +11,33 @@
 <div class="h-20" />
 <!-----------       LEFT MENU       ------------>
 <LeftSidebar page={"contact"} />
+<!------       CONTENT CONTAINER     ------------>
+<div class="min-h-screen">
+  <div class="flex justify-center mt-20">
+    <!-- CONTACT INFO  -->
+    <div class="w-1/3">
+      <h1 class="text-neutral-200 text-center font-bold text-xl">
+        Contact info:
+      </h1>
+      <div class="text-neutral-200 text-lg mt-4"><i class="fa-solid fa-envelope" />
+      <span class="ml-2">peertuber@email.com</span>
+      </div>
+      <div class="text-neutral-200 text-2xl mt-4 text-center">
+        <a href="https://www.linkedin.com/in/andrei-zheksim/" target="_blank">
+
+          <i class="fa-brands fa-linkedin mr-2 hover:text-neutral-100"></i>
+        </a>
+        <a href="https://github.com/acdmft" target="_blank">
+
+          <i class="fa-brands fa-square-github hover:text-neutral-100"></i>
+        </a>
+      </div>
+    </div>
+    <!-- CONTACT FORM -->
+    <div
+      class="w-2/3 flex justify-center"
+    >
+      <ContactForm />
+    </div>
+  </div>
+</div>
