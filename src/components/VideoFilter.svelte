2d7c925bb3bee7927698ce696ad0e1ab3cbab494
fusbudget <andrewsanser@gmail.com>
2022-09-23 11:31:36 +0200

    add improvements in VidoeFilter: button => li, class:active={...}

--- src/components/VideoFilter.svelte
+++ src/components/VideoFilter.svelte
@@ -1,16 +1,14 @@
 <script lang="ts">
   import { createEventDispatcher } from "svelte";
+  let catFilters = ["all","Science & Technologies","gaming","education","News & Politics","music","entertainment"];
+
+
 
 
   let activeFilter = "all";
   const dispatch = createEventDispatcher();
   // HIGHLIGHT FILTER ITEM
   const highlightFilterItem = (e) => {
-    let filterMenu = document.querySelector("nav");
-    for (let i = 0; i < filterMenu.childElementCount; i++) {
-      filterMenu.children[i].classList.remove("active");
-    }
-    e.target.classList.add("active");
     activeFilter = e.target.id;
   };
   const handleClick = (e) => {
@@ -25,13 +23,12 @@
 
 <div id="categories" class="h-14 grid content-center justify-items-center">
   <nav class="flex justify-around  w-5/6">
-     <button on:click={(e)=> handleClick(e)} id="all" class="w-fit h-fit rounded-lg text-neutral-200 my-auto px-2 py-1 outline outline-1 outline-gray-200 active"> All </button>
-     <button on:click={(e)=> handleClick(e)} id="science" class="w-fit h-fit rounded-lg text-neutral-200 my-auto px-2 py-1 outline outline-1 outline-gray-200">Science & Technologies </button>
-     <button on:click={(e)=> handleClick(e)} id="gaming" class="w-fit h-fit rounded-lg text-neutral-200 my-auto px-2 py-1 outline outline-1 outline-gray-200">Gaming</button>
-     <button on:click={(e)=> handleClick(e)} id="education" class="w-fit h-fit rounded-lg text-neutral-200 my-auto px-2 py-1 outline outline-1 outline-gray-200">Education</button>
-     <button on:click={(e)=> handleClick(e)} id="politics" class="w-fit h-fit rounded-lg text-neutral-200 my-auto px-2 py-1 outline outline-1 outline-gray-200">News & Politics</button>
-     <button on:click={(e)=> handleClick(e)} id="music" class="w-fit h-fit rounded-lg text-neutral-200 my-auto px-2 py-1 outline outline-1 outline-gray-200">Music</button>
-     <button on:click={(e)=> handleClick(e)} id="entertainment" class="w-fit h-fit rounded-lg text-neutral-200 my-auto px-2 py-1 outline outline-1 outline-gray-200">Entertainment</button>
+    <ul>
+      {#each catFilters as catFilter}
+      <li on:click={(e)=> handleClick(e)} id={catFilter} class:active={catFilter === activeFilter} class="float-left ml-4 hover:cursor-pointer hover:text-neutral-400 w-fit h-fit rounded-lg text-neutral-200 my-auto px-2 py-1 outline outline-1 outline-gray-200 "> {catFilter} </li>
+      {/each}
+    </ul>
+    
   </nav>
 
 </div>
@@ -39,8 +36,5 @@
 <style>
   nav .active {
     background-color: #F472B6;
-  }
-  nav button:hover {
-    background-color: #3F3F46;   /* zinc 700 */
   }
 </style>
