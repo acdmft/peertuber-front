48cc427f5bc06cf7617c83fc783429151ac38a95
fusbudget <andrewsanser@gmail.com>
2022-09-24 14:06:29 +0200

    add retrVideos to fetch videos

---
+++ src/lib/videos.ts
@@ -0,0 +1,29 @@
+// functions that retrieve video previews from the server 
+
+// called from Home page 
+export async function retrVideos(url, cat) {
+  const query = {
+    query: `{ videos(category: "${cat}") { 
+      instance {
+        host
+        name
+      }
+      _id
+      name 
+      url 
+      thumbnailImg 
+      likes
+      duration
+    }}`,
+  };
+  const res = await fetch(`${url}/data`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(query),
+  });
+  const result = await res.json();
+  let recVid = result.data.videos;
+  return recVid;
+}
