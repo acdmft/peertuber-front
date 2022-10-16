9ec121aa61294459145e9eb39182174508ff145f
fusbudget <andrewsanser@gmail.com>
2022-10-16 14:22:33 +0200

    add w-11/12 in VideoRow

--- src/components/VideoRow.svelte
+++ src/components/VideoRow.svelte
@@ -10,11 +10,12 @@
   export let stripCards;
   $: stripCardsArr = Array(stripCards).fill("");
   // let widthVars = {1: "11/12", 3: ""}
-  const {width, height} = getCardProps(cardNum);
-  
+  const { width, height } = getCardProps(cardNum);
 </script>
 
-<div class="flex justify-around justify-self-center w-full mt-4 md:mt-12 lg:w-11/12 md:ml-12">
+<div
+  class="flex justify-around justify-self-center w-11/12 mt-4 md:mt-12 lg:w-11/12 md:ml-12"
+>
   {#each cardsData as card}
     {#if page === "home"}
       <Card
