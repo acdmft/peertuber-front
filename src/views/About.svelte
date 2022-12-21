b6654ed392e236080cc3f0ca994e02a58a4ac09f
meep <crankyfox@protonmail>
2022-12-21 19:26:50 +0100

    add improvements in About page

--- src/views/About.svelte
+++ src/views/About.svelte
@@ -19,8 +19,8 @@
 <div class="min-h-screen">
   <div class="mt-8 lg:mt-20 w-full px-2 text-sm md:text-regular lg:text-xl md:w-4/5 lg:w-1/2 mx-auto text-neutral-200 font-semibold text-left">
     <h1 class="text-2xl font-bold">About project: </h1>
-    <p class="mt-4"><a class="text-blue-400 hover:text-blue-500" href="https://joinpeertube.org" target="_blank" rel="noreferrer">Peertube</a> is open source software for hosting videos, it is based on p2p technology that reduces load on individual servers while they distribute the videos.
-      Development started in 2017 by the programmer known as Chocobozzz. Now it is supported by the french non-profit organization <a class="text-blue-400 hover:text-blue-500" href="https://framasoft.org/en/" target="_blank" rel="noreferrer">Framasoft.</a>
+    <p class="mt-4"><a class="text-blue-400 hover:text-blue-500" href="https://joinpeertube.org" target="_blank" rel="noreferrer">Peertube</a> is an open source software for hosting videos, it is based on p2p technology that reduces load on individual servers while they distribute the videos.
+      Project development  started in 2017 by the programmer known as Chocobozzz. Later the maintainace was taken by the french non-profit organization <a class="text-blue-400 hover:text-blue-500" href="https://framasoft.org/en/" target="_blank" rel="noreferrer">Framasoft.</a>
     </p>
     <p class="mt-4">
       Anyone can install Peertube on the sever, upload and then share videos. Streaming is also possible. The servers running the software are united in the federation of instances, this is possible thanks to <a class="text-blue-400 hover:text-blue-500" href="https://activitypub.rocks/" target="_blank" rel="noreferrer"> ActivityPub protocol.</a></p>
