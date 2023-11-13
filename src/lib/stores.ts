03ab0ffeee3a1e16dc3d22f4e861e720b78f1706
gilert <crankyfox@protonmail.com>
2023-11-13 18:52:18 +0100

    feat: add store isAppLoaded to put in condition for previeMessage

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,6 +1,8 @@
 import { writable } from 'svelte/store';
 
 export const count = writable(0);
+
+export const isAppLoaded = writable(false)
 // user's state (logged in or not)
 export const user = writable<boolean>(localStorage.user === 'true');
 user.subscribe((val) => localStorage.user = String(val));
